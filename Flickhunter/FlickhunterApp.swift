41b459befbea84facd2e42a532b5c770f03e4533
Abelardo Marquez <abelardo@hey.com>
2021-03-19 14:52:29 +0100

    Add Folder Structure

---
+++ Flickhunter/FlickhunterApp.swift
@@ -0,0 +1,17 @@
+//
+//  flickhunterApp.swift
+//  flickhunter
+//
+//  Created by Abelardo Marquez on 19.03.21.
+//
+
+import SwiftUI
+
+@main
+struct FlickhunterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
